082abceaeda1ae89853e42e85b09cabc27dabce3
gangwarsumit <sumitkurmi1000@gmail.com>
2020-04-19 00:10:43 +0530

    internal and external storage

---
+++ app/src/main/java/com/example/ca2cse225/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.ca2cse225;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button b1;
+    Button b2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1= (Button) findViewById(R.id.B1);
+        b2=(Button) findViewById(R.id.B2);
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.B1:
+                Intent intent=new Intent(this,Internal.class);
+                startActivity(intent);
+                break;
+            case R.id.B2:
+                Intent intent1=new Intent(this,External.class);
+                startActivity(intent1);
+                break;
+        }
+    }
+}
